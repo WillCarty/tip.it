fb26a3d5a928990ca054ff6395369f4555b30559
William Carty <will.carty@interapthq.com>
2016-09-26 21:22:31 -0400

    working on party of diners

--- tipCal.js
+++ tipCal.js
@@ -8,40 +8,27 @@ function EbillAmount() {
 }
 
 //*******************Click event excellent service
+//*******************Click event good service
+//*******************Click event average service
+//*******************Click event bad service
 $(function () {
     $("#excellent").on("click", EbillAmount);
-
+    $("#bad").on("click", BbillAmount);
+    $("#good").on("click", GbillAmount);
+    $("#average").on("click", AbillAmount);
 });
-
-
 function GbillAmount() {
     var Gtotal = $("#billAmount").val();
     var GnewTotal = Gtotal * .2
     grandTotal = +Gtotal + +GnewTotal
     //partyAmount();
 }
-
-//*******************Click event good service
-$(function () {
-    $("#good").on("click", GbillAmount);
-
-});
-
-
 function AbillAmount() {
     var Atotal = $("#billAmount").val();
     var AnewTotal = Atotal * .15
     grandTotal = +Atotal + +AnewTotal
     //partyAmount();
 }
-
-//*******************Click event average service
-$(function () {
-    $("#average").on("click", AbillAmount);
-
-});
-
-
 function BbillAmount() {
     var Btotal = $("#billAmount").val();
     var BnewTotal = Btotal * .10
@@ -49,12 +36,13 @@ function BbillAmount() {
    // partyAmount();
 }
 
-//*******************Click event bad service
-$(function () {
-    $("#bad").on("click", BbillAmount);
 
-});
+function partyAmount(){
+if(document.getElementById('2diner').checked) {
+    var twoDiners = grandTotal / 2;
+    var newDinercard= $("#newCard2").html;
+    newDinercard = newDinercard.replace("@@grandTotal@@",Math.round(twoDiners));
 
-//if("#2diner"=== checked){
-  // grandTotal / 2 
-//}
+    
+}
+}
