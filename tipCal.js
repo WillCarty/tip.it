05e6ce5e5f8a868fc54ba08d7d6a1b45a3cfe62b
William Carty <will.carty@interapthq.com>
2016-09-27 13:02:36 -0400

    final

--- tipCal.js
+++ tipCal.js
@@ -1,48 +1,85 @@
 var grandTotal;
 
-function EbillAmount() {
-    var Etotal = $("#billAmount").val();
-    var EnewTotal = Etotal * .25
-    grandTotal = +Etotal + +EnewTotal
-  //partyAmount()
-}
+
+
+
 
 //*******************Click event excellent service
 //*******************Click event good service
 //*******************Click event average service
 //*******************Click event bad service
-$(function () {
-    $("#excellent").on("click", EbillAmount);
-    $("#bad").on("click", BbillAmount);
-    $("#good").on("click", GbillAmount);
-    $("#average").on("click", AbillAmount);
-});
+
+function clickHandler () {
+  if(document.getElementById('excellent').checked) {
+      EbillAmount();
+  }
+  else if(document.getElementById('good').checked){
+      GbillAmount();
+
+  }
+  else if(document.getElementById('average').checked){
+      AbillAmount();
+  }
+  else if(document.getElementById('bad').checked){
+      BbillAmount();
+  } 
+}
+function EbillAmount() {
+    var Etotal = $("#billAmount").val();
+    var EnewTotal = Etotal * .25
+    grandTotal = +EnewTotal
+  partyAmount()
+}
 function GbillAmount() {
     var Gtotal = $("#billAmount").val();
-    var GnewTotal = Gtotal * .2
-    grandTotal = +Gtotal + +GnewTotal
-    //partyAmount();
+    var GnewTotal = Gtotal * .20
+    grandTotal = +GnewTotal
+    partyAmount();
 }
 function AbillAmount() {
     var Atotal = $("#billAmount").val();
     var AnewTotal = Atotal * .15
-    grandTotal = +Atotal + +AnewTotal
-    //partyAmount();
+    grandTotal = +AnewTotal
+    partyAmount();
 }
 function BbillAmount() {
     var Btotal = $("#billAmount").val();
-    var BnewTotal = Btotal * .10
-    grandTotal = +Btotal + +BnewTotal
-   // partyAmount();
+    var BnewTotal = Btotal * .10;
+    grandTotal = +BnewTotal;
+    partyAmount();
 }
 
 
 function partyAmount(){
 if(document.getElementById('2diner').checked) {
-    var twoDiners = grandTotal / 2;
-    var newDinercard= $("#newCard2").html;
-    newDinercard = newDinercard.replace("@@grandTotal@@",Math.round(twoDiners));
-
+    var twoDiners = Math.round(grandTotal / 2);
+    textbox = document.getElementById('grandTotaltext');
+    textbox.value ="$" + twoDiners + "." + +0 + +0 ;
+    
     
 }
+else if(document.getElementById('3diner').checked){
+    var threeDiners = Math.round(grandTotal / 3);
+    textbox = document.getElementById('grandTotaltext');
+    textbox.value = "$" + threeDiners + "." + +0 + +0 ; ;
+
 }
+
+else if(document.getElementById('4diner').checked){
+    var fourDiners = grandTotal / 4;
+    textbox = document.getElementById('grandTotaltext');
+    textbox.value = "$" + fourDiners + "." + +0 + +0 ; ;
+}
+else if(document.getElementById('5diner').checked){
+    var fiveDiners = Math.round(grandTotal / 5);
+    textbox = document.getElementById('grandTotaltext');
+    textbox.value = "$" + fiveDiners + "." + +0 + +0 ; ;
+}
+}
+$(function () {
+    $("#tipIt").on("click", clickHandler);
+     isNumberKey();
+});
+
+
+
